71c9e656a5a02e6c9299e98d6bfd69d21459709c
sxunxin <whang31658718@gmail.com>
2025-07-22 02:58:23 +0900

    style: 파일 정리 및 헤더 디자인 수정
    - 파일들을 역할에 맞게 폴더에 정리
    - 헤더 디자인 아이콘 비율 수정

--- app/src/main/java/com/example/blackstone/shopping/ShoppingFragment.kt
+++ app/src/main/java/com/example/blackstone/shopping/ShoppingFragment.kt
@@ -1,10 +1,11 @@
-package com.example.blackstone
+package com.example.blackstone.shopping
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.blackstone.R
 
 class ShoppingFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
